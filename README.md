53364c30acc237dddc767f1a349cd8b9b8352ba9
ngobenihc <ngobenihc@yahoo.com>
2024-03-17 11:17:12 +0200

    day5

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # 100daysofALXSE
 coding challenge for 100days
 
+basic code
 day 1 welcome code
 day 2 guessing number code
 day finding the same and avarage 
---
+++ day5/src/FizzBuzz.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+public class FizzBuzz {
+    public String checkNumber(int number) {
+        boolean divisibleBy3 = number % 3 == 0;
+        boolean divisibleBy5 = number % 5 == 0;
+
+        if (( divisibleBy3) && (divisibleBy5 )) {
+            return "FizzBuzz";
+        }
+        else if (divisibleBy3){
+            return "Fizz";
+        }
+        else  if (divisibleBy5){
+            return "Buzz";
+        }
+        else {
+            return String.valueOf(number);
+        }
+    }
+
+    public String countTo(int number) {
+        ArrayList<String> store_num = new ArrayList<>();
+        for(int h =1;h<= number;h++){
+            store_num.add(checkNumber(h));
+        }
+        return store_num.toString();
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner input  = new Scanner(System.in);
+
+        int number;
+        System.out.print("enter a number: ");
+        number = input.nextInt();
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.countTo(number));
+
+
+    }
+}

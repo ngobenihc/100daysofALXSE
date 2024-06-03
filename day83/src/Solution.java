670fa8dc51ab22d201a21f8fb5a66898bf33f86a
ngobenihc <ngobenihc@yahoo.com>
2024-06-03 09:15:58 +0200

    day83

---
+++ day83/src/Solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (Method method : methods) {
+                if (set.contains(method.getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(method.getName());
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class Prime {
+    public void checkPrime(int... numbers) {
+        for (int number : numbers) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}

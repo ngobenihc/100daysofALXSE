bbe3019fb2692279e9345fac2d2c377a9c354875
ngobenihc <ngobenihc@yahoo.com>
2024-06-13 10:34:23 +0200

    day93

---
+++ day93/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class BiCycle {
+    String define_me() {
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle {
+    @Override
+    String define_me() {
+        return "a cycle with an engine.";
+    }
+
+    MotorCycle() {
+        System.out.println("Hello I am a motorcycle, I am " + define_me());
+        String temp = super.define_me(); // Call the superclass method
+        System.out.println("My ancestor is a cycle who is " + temp);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        MotorCycle M = new MotorCycle();
+    }
+}
